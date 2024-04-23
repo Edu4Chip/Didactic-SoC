196e9bed3d6bace5746adca731854b96869e8911
Matti Käyrä <matti.kayra@tuni.fi>
2024-04-23 12:48:22 +0300

    relocate ss3 wrapper files, create 3rd modulefile

--- src/rtl/Student_SS_2.sv
+++ src/rtl/Student_SS_2.sv
@@ -1,38 +1,38 @@
 //-----------------------------------------------------------------------------
-// File          : Student_SS_2.v
-// Creation date : 20.02.2024
-// Creation time : 09:44:58
+// File          : student_ss_2.v
+// Creation date : 22.04.2024
+// Creation time : 14:06:50
 // Description   : 
 // Created by    : 
 // Tool : Kactus2 3.13.1 64-bit
 // Plugin : Verilog generator 2.4
-// This file was generated based on IP-XACT component tuni.fi:subsystem.wrapper:Student_SS_2:1.0
-// whose XML file is C:/Users/kayra/Documents/repos/tau-ipxact/ipxact/tuni.fi/subsystem.wrapper/Student_SS_2/1.0/Student_SS_2.1.0.xml
+// This file was generated based on IP-XACT component tuni.fi:subsystem:student_ss_2:1.0
+// whose XML file is C:/Users/kayra/Documents/repos/didactic-soc/ipxact/tuni.fi/subsystem/student_ss_2/1.0/student_ss_2.1.0.xml
 //-----------------------------------------------------------------------------
 
-module Student_SS_2(
+module student_ss_2(
     // Interface: APB
-    input  logic [31:0] PADDR,
-    input  logic        PENABLE,
-    input  logic        PSEL,
-    input  logic [31:0] PWDATA,
-    input  logic        PWRITE,
-    output logic [31:0] PRDATA,
-    output logic        PREADY,
-    output logic        PSLVERR,
+    input  logic         [31:0]         PADDR,
+    input  logic                        PENABLE,
+    input  logic         [3:0]          PSEL,
+    input  logic         [31:0]         PWDATA,
+    input  logic                        PWRITE,
+    output logic         [31:0]         PRDATA,
+    output logic         [3:0]          PREADY,
+    output logic         [3:0]          PSELERR,
 
     // Interface: Clock
-    input  logic        clk,
+    input  logic                        clk_in,
 
     // Interface: IRQ
-    output logic        irq,
+    output logic                        irq_2,
 
     // Interface: Reset
-    input  logic        reset_int,
+    input  logic                        reset_int,
 
     // Interface: SS_Ctrl
-    input logic        irq_en_2,
-    input logic [7:0]  ss_ctrl_2
+    input  logic                        irq_en_2,
+    input  logic         [7:0]          ss_ctrl_2
 );
 
 // WARNING: EVERYTHING ON AND ABOVE THIS LINE MAY BE OVERWRITTEN BY KACTUS2!!!
@@ -43,5 +43,6 @@ assign PSELERR = 'd0;
 assign PSREADY = 'd0;
 assign PRDATA  = 'd0;
 assign irq     = 'd0;
+
 
 endmodule
--- src/rtl/Student_SS_3.sv
+++ src/rtl/Student_SS_3.sv
@@ -1,39 +1,47 @@
 //-----------------------------------------------------------------------------
-// File          : student_ss_2.v
-// Creation date : 22.04.2024
-// Creation time : 14:06:50
+// File          : Student_SS_3.v
+// Creation date : 23.04.2024
+// Creation time : 12:45:24
 // Description   : 
 // Created by    : 
 // Tool : Kactus2 3.13.1 64-bit
 // Plugin : Verilog generator 2.4
-// This file was generated based on IP-XACT component tuni.fi:subsystem:student_ss_2:1.0
-// whose XML file is C:/Users/kayra/Documents/repos/didactic-soc/ipxact/tuni.fi/subsystem/student_ss_2/1.0/student_ss_2.1.0.xml
+// This file was generated based on IP-XACT component tuni.fi:subsystem:Student_SS_3:1.0
+// whose XML file is C:/Users/kayra/Documents/repos/didactic-soc/ipxact/tuni.fi/subsystem/Student_SS_3/1.0/Student_SS_3.1.0.xml
 //-----------------------------------------------------------------------------
 
-module student_ss_2(
+module Student_SS_3(
     // Interface: APB
     input  logic         [31:0]         PADDR,
     input  logic                        PENABLE,
-    input  logic         [3:0]          PSEL,
+    input  logic                        PSEL,
     input  logic         [31:0]         PWDATA,
     input  logic                        PWRITE,
     output logic         [31:0]         PRDATA,
-    output logic         [3:0]          PREADY,
-    output logic         [3:0]          PSELERR,
+    output logic                        PREADY,
+    output logic                        PSLVERR,
 
     // Interface: Clock
     input  logic                        clk_in,
 
     // Interface: IRQ
-    output logic                        irq_2,
+    output logic                        irq_3,
 
     // Interface: Reset
     input  logic                        reset_int,
 
     // Interface: SS_Ctrl
-    input  logic                        irq_en_2,
-    input  logic         [7:0]          ss_ctrl_2
+    input  logic                        irq_en_3,
+    input  logic         [7:0]          ss_ctrl_3
 );
 
 // WARNING: EVERYTHING ON AND ABOVE THIS LINE MAY BE OVERWRITTEN BY KACTUS2!!!
+
+// TODO: Replace this with your module impleemtnation
+assign PSELERR = 'd0;
+assign PSREADY = 'd0;
+assign PRDATA  = 'd0;
+assign irq     = 'd0;
+
+
 endmodule
