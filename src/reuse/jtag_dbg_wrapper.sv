93ec8a6628966d0b98b475b729fb982bb9f318ca
Roni Hämäläinen <roni.hamalainen@tuni.fi>
2024-04-30 10:52:55 +0300

    Add missing include

--- src/reuse/jtag_dbg_wrapper.sv
+++ src/reuse/jtag_dbg_wrapper.sv
@@ -20,6 +20,9 @@
     * original module implementation by AN and TZS
     * tweaks for importing to kactus2 by MK
 */
+
+`include "dm_pkg.sv"
+
 module jtag_dbg_wrapper #(
     parameter                              AXI_AW           = 32,
     parameter                              AXI_DW           = 32,
