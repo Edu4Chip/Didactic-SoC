3685e06074050c1c673c7c8373cc96dc76c4eb45
Roni Hämäläinen <roni.hamalainen@tuni.fi>
2024-04-30 13:49:22 +0300

    Update verilator input paths

--- verification/verilator/run.sh
+++ verification/verilator/run.sh
@@ -21,6 +21,33 @@ verilator \
     -I./ips/common_cells/include \
     -I./ips/common_cells/src \
     -I./ips/common_cells/src/deprecated \
+    ips/riscv-dbg/src/dm_pkg.sv \
+    ips/common_cells/src/cdc_reset_ctrlr_pkg.sv \
+    ips/common_cells/src/cdc_4phase.sv \
+    ips/tech_cells_generic/src/rtl/tc_clk.sv \
+    ips/apb_gpio/rtl/apb_gpio.sv \
+    src/rtl/APB_SDIO_tieoff.v \
+    ips/apb_spi_master/apb_spi_master.sv \
+    ips/apb_spi_master/spi_master_apb_if.sv \
+    ips/axi_spi_master/spi_master_fifo.sv \
+    ips/axi_spi_master/spi_master_controller.sv \
+    ips/axi_spi_master/spi_master_clkgen.sv \
+    ips/axi_spi_master/spi_master_tx.sv \
+    ips/axi_spi_master/spi_master_rx.sv \
+    ips/apb_uart/src/apb_uart.sv \
+    ips/apb_uart/src/slib_input_sync.sv \
+    ips/apb_uart/src/slib_input_filter.sv \
+    ips/apb_uart/src/uart_interrupt.sv \
+    ips/apb_uart/src/slib_edge_detect.sv \
+    ips/apb_uart/src/uart_baudgen.sv \
+    ips/apb_uart/src/slib_clock_div.sv \
+    ips/apb_uart/src/slib_fifo.sv \
+    ips/apb_uart/src/uart_transmitter.sv \
+    ips/apb_uart/src/uart_receiver.sv \
+    ips/apb_uart/src/slib_counter.sv \
+    ips/apb_uart/src/slib_mv_filter.sv \
+    ips/axi/src/axi_pkg.sv \
+    src/rtl/AX4LITE_APB_converter_wrapper.sv \
     src/generated/Didactic.v
 verilator_exit_code=$?
 echo ""
