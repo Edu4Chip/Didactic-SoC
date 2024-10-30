b354ff08a01217647d15b35fa10e472c5b6c3cdc
Antti Nurmi <antti.nurmi@tuni.fi>
2024-10-30 12:52:00 +0200

    Modify script to use bender path

--- verification/verilator/scripts/do_fix.sh
+++ verification/verilator/scripts/do_fix.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+AXI_PATH="$(bender path axi)"
+
 # Comment out the failing code
-sed -i '125 i /*' ips/axi/src/axi_lite_mux.sv
-sed -i '128 i */' ips/axi/src/axi_lite_mux.sv
+sed -i '125 i /*' $AXI_PATH/src/axi_lite_mux.sv
+sed -i '128 i */' $AXI_PATH/src/axi_lite_mux.sv
 
 # Add working code
-sed -i '129 i // FIXME: previous does not work: see verilator problem 1' ips/axi/src/axi_lite_mux.sv
-sed -i '130 i // This value is derived from file SysCtrl_xbar.sv parameter AXI4LITE_INITIATORS' ips/axi/src/axi_lite_mux.sv
-sed -i '131 i localparam NoSlvPorts_fix_clog2_minus1 = 1;' ips/axi/src/axi_lite_mux.sv
-sed -i '132 i typedef logic [NoSlvPorts_fix_clog2_minus1:0] select_t;' ips/axi/src/axi_lite_mux.sv
+sed -i '129 i // FIXME: previous does not work: see verilator problem 1' $AXI_PATH/src/axi_lite_mux.sv
+sed -i '130 i // This value is derived from file SysCtrl_xbar.sv parameter AXI4LITE_INITIATORS' $AXI_PATH/src/axi_lite_mux.sv
+sed -i '131 i localparam NoSlvPorts_fix_clog2_minus1 = 1;' $AXI_PATH/src/axi_lite_mux.sv
+sed -i '132 i typedef logic [NoSlvPorts_fix_clog2_minus1:0] select_t;' $AXI_PATH/src/axi_lite_mux.sv
