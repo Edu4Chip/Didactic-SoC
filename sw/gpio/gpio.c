78b560cce7a5c3bb94778e10d3ba2cffb725be0d
Matti Käyrä <matti.kayra@tuni.fi>
2024-11-19 14:48:41 +0200

    [sw] Clean up GPIO test code from debug features.

--- sw/gpio/gpio.c
+++ sw/gpio/gpio.c
@@ -14,46 +14,38 @@
 int main(){
   const uint32_t all_1 = 0xFFFFFFFF;
   const uint32_t all_0 = 0x0;
-  volatile uint32_t read_val = 0;
+  volatile uint32_t read_val = 0xFFFF;
   volatile uint32_t errors = 0;
 
   //uart_init();
 
-  // write one over to see if function behaves correctly
-  for(volatile uint32_t i=0; i < 7; i++){
+  // Test too many gpios to check that init
+  // function does not break
+  for(volatile uint32_t i=0; i < 9; i++){
     gpio_init_in(i);
   }
-  // test too large val to see it does not break
-  gpio_init_in(8);
-
+  
+  // check value
   read_val = gpio_read();
 
-  if(read_val == 0){
+  // tri1 does not force gpios internally, 
+  // so read should be equal to 0
+  if(read_val != 0x0){
     errors++;
     //uart_print("[Error]");
   }
 
+  // Again, check that init function can handle overflow
   for(uint32_t i=0; i < 9; i++){
     gpio_init_out(i);
   }
-  // test too large val to see it does not break
-  gpio_init_out(8);
 
+  // blinker waveform - check manually
   gpio_write(all_1);
   gpio_write(all_0);
   gpio_write(all_1);
   gpio_write(all_0);
-  
-  // test to figure out why exception
-  read_val = PAD_CFG_GPIO6;
-  read_val = PAD_CFG_GPIO7;
-  PAD_CFG_GPIO7 = 0;
-  PAD_CFG_GPIO7 = 0x1;
-  PAD_CFG_GPIO7 = 0;
-  
-
-  gpio_init_in(7);
-
+ 
   return errors;
 
 }
