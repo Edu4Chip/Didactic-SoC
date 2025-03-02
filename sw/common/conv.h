89b69eb2e7e20d1193120d092151faa4cd1181bf
Antti Nurmi <antti.nurmi@tuni.fi>
2025-03-02 14:59:23 +0200

    Add conv.h

---
+++ sw/common/conv.h
@@ -0,0 +1,18 @@
+/*
+ * Name: conv.h
+ * Contributor(S):
+ *    - Antti Nurmi <antti.nurmi@tuni.fi>
+ * Description:
+ *    - helper functions conv_ex test
+ */
+#ifndef __CONV_H__
+#define __CONV_H__
+
+#include <stdlib.h>
+
+void init_matrix (uint8_t dimension, uint8_t mat[dimension][dimension], uint32_t base){
+    mat[0][0] = 1;
+    //return matrix;
+}
+
+#endif //__CONV_H__
--- sw/conv_ex/conv_ex.c
+++ sw/conv_ex/conv_ex.c
@@ -6,6 +6,7 @@
  *    - (Mock) 2D convolution application for course project
  */
 #include "uart.h"
+#include "conv.h"
 
 int main() {
 
@@ -15,8 +16,19 @@ int main() {
 
   fprint("hello from 2D conv, COMP.CE.%x!\n", test_code);
 
-  volatile uint32_t DMEM_FIRST = *(uint32_t*)(0x01010000);
+  volatile uint32_t DMEM_FIRST  = *(uint32_t*)(0x01010000);
+  volatile uint32_t DMEM_SECOND = *(uint32_t*)(0x0101000C);
+
+  uint32_t mult_res = DMEM_FIRST * DMEM_SECOND;
+
+  uint8_t mat_test[4][4] = {{0}}; 
+  init_matrix(4, mat_test, 0x01010000);
+  fprint("matrix test: %x\n", mat_test[0][0]);
+
   fprint("DMEM first value: %x\n", DMEM_FIRST);
+  fprint("DMEM second value: %x\n", DMEM_SECOND);
+  fprint("mul_res: %x\n", mult_res);
+
 
   
   return 0;
