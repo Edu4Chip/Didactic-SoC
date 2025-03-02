4663059fb31ba4aceb10b7e4fad1ed058740e344
Antti Nurmi <antti.nurmi@tuni.fi>
2025-03-02 18:15:36 +0200

    Add WIP

--- sw/common/conv.h
+++ sw/common/conv.h
@@ -8,7 +8,7 @@
 #ifndef __CONV_H__
 #define __CONV_H__
 
-#include <stdlib.h>
+//#include <stdlib.h>
 
 void init_matrix (uint8_t dimension, uint8_t mat[dimension][dimension], uint32_t base){
     //mat[0][0] = 1;
@@ -21,9 +21,11 @@ void init_matrix (uint8_t dimension, uint8_t mat[dimension][dimension], uint32_t
     }
     */
     for (int i=0; i<dimension; i++){
-        mat[i][0] = *(uint32_t*)(base);
+        for (int j=0; i<dimension; j++){
+            mat[i][j] = *(uint32_t*)(base);
+            //mat[i][0] = *(uint32_t*)(base);
+        }
     }
-
 }
 
 #endif //__CONV_H__
