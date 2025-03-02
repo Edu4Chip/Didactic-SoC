cc235457e75b4763c2647809fc055006285c0e43
Antti Nurmi <antti.nurmi@tuni.fi>
2025-03-02 16:14:45 +0200

    Fix linking issue

--- sw/common/conv.h
+++ sw/common/conv.h
@@ -13,11 +13,17 @@
 void init_matrix (uint8_t dimension, uint8_t mat[dimension][dimension], uint32_t base){
     //mat[0][0] = 1;
     //return matrix;
+    /*
     for (int i=0; i<dimension; i++){
         for (int j=0; i<dimension; j++){
             mat[i][j] = *(uint32_t*)(base);
         }
     }
+    */
+    for (int i=0; i<dimension; i++){
+        mat[i][0] = *(uint32_t*)(base);
+    }
+
 }
 
 #endif //__CONV_H__
