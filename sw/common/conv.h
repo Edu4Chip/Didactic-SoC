99f6148aabdaf8fe5a517a0c51874146735dfbed
Antti Nurmi <antti.nurmi@tuni.fi>
2025-03-04 16:00:03 +0200

    Add WIP

--- sw/common/conv.h
+++ sw/common/conv.h
@@ -9,12 +9,32 @@
 #define __CONV_H__
 
 void init_matrix (uint8_t dim, uint8_t mat[dim][dim], uint32_t base){
-    for (int i=0; i<dim; i++){
-        for (int j=0; j<dim; j++){
-            mat[i][j] = 1; //*(volatile uint32_t*)(base);
-            //mat[i][0] = *(uint32_t*)(base);
+    uint8_t size = dim*dim;
+    fprint("size: %x\n", size);
+    for (int offset=0; offset<size; offset += 4){
+        volatile uint32_t word_line = *(uint32_t*)(base + offset);
+        uint8_t bytes[4]; // extract big-endian bytes
+        bytes[0] =   ((word_line>>24)&0xff);             // move byte 3 to byte 0
+        bytes[2] =   ((word_line<<8)&0xff0000)    >> 16; // move byte 1 to byte 2
+        bytes[1] =   ((word_line>>8)&0xff00)      >> 8;  // move byte 2 to byte 1
+        bytes[3] =   ((word_line<<24)&0xff000000) >> 24; // byte 0 to byte 3
+        for (int pos = 0; pos < size; pos++){
+
+            int x = pos / dim;
+            int y = pos % dim;
+
+            fprint("bytes: %x, pos: %x, offset: %x\n", bytes[pos], pos, offset);
+            mat[x][y] = bytes[pos];//big_endian << pos*8;
         }
+        
     }
+    //for (int i=0; i<dim; i++){
+    //    uint32_t data_line = *(uint32_t*)(base + 4*i);
+    //    for (int j=0; j<dim; j++){
+    //        mat[i][j] = *(uint32_t*)(base + 4*j + i);
+    //        //mat[i][0] = *(uint32_t*)(base);
+    //    }
+    //}
 }
 
 void print_matrix (uint8_t dim, uint8_t mat[dim][dim]){
--- sw/conv_ex/conv_ex.c
+++ sw/conv_ex/conv_ex.c
@@ -22,15 +22,13 @@ int main() {
   uint32_t mult_res = DMEM_FIRST * DMEM_SECOND;
 
   uint8_t mat_test[4][4] = {{0}}; 
+  uint8_t wgt_test[2][2] = {{0}}; 
   init_matrix(4, mat_test, 0x01010000);
+  init_matrix(2, wgt_test, 0x01010000 + 0x40);
   fprint("matrix test:\n");
   print_matrix(4, mat_test);
-
-  fprint("DMEM first value: %x\n", DMEM_FIRST);
-  fprint("DMEM second value: %x\n", DMEM_SECOND);
-  fprint("mul_res: %x\n", mult_res);
+  print_matrix(2, wgt_test);
 
 
-  
   return 0;
 }
