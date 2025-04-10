1dc9963fd13a14a1dd2b72ac715b454970fafaa6
Matti Käyrä (TAU) <matti.kayra@tuni.fi>
2025-04-10 13:06:21 +0300

    [ipxact/rtl] and ss hierarchical layer and registers to top level. Start conversion to more generic 16x GPIO setup

---
+++ src/rtl/analog_status_array.v
@@ -0,0 +1,103 @@
+/*
+  Contributors:
+    * Matti Käyrä (matti.kayra@tuni.fi)
+  Description:
+    * status registers for analog system
+*/
+
+module analog_status_array(
+    // Interface: APB
+    input  logic         [15:0]         PADDR,
+    input  logic                        PENABLE,
+    input  logic                        PSEL,
+    input  logic         [3:0]          PSTRB,
+    input  logic         [31:0]         PWDATA,
+    input  logic                        PWRITE,
+    output logic         [31:0]         PRDATA,
+    output logic                        PREADY,
+    output logic                        PSLVERR,
+
+    // Interface: clk
+    input  logic                        clk_in,
+
+    // Interface: reset
+    input  logic                        reset_n,
+
+    // Interface: status_0
+    input                [31:0]         status_0,
+
+    // Interface: status_1
+    input                [31:0]         status_1,
+
+    // Interface: status_2
+    input                [31:0]         status_2,
+
+    // Interface: status_3
+    input                [31:0]         status_3
+);
+
+  // APB out regs
+  logic PSLVERR_reg;
+  logic [31:0] PRDATA_reg;
+  logic PREADY_reg;
+
+  logic [31:0 ]status_0_reg;
+  logic [31:0] status_1_reg;
+
+  always_ff @(posedge clk_in or negedge reset_n)
+  apb_proc: begin
+    if (~rst_n) begin
+      status_0_reg = 'h0;
+      status_1_reg = 'h0;
+    end
+    else begin
+        if(PSEL) begin
+            if (PREADY_reg == 1 && PSLVERR_reg == 1 ) begin
+              PSLVERR_reg <= 1'b0;
+              PREADY_reg <= 1'b0;
+            end
+            else if (PREADY_reg == 1) begin
+              PREADY_reg <= 1'b0;
+            end
+            else if (PWRITE) begin
+                PREADY_reg  <= 1'b1;
+                PSLVERR_reg <= 1'b1;
+            end
+            else if (~PWRITE) begin    
+                if (PADDR == 0) begin
+                    PRDATA_reg <= status_0_reg;
+                    PSLVERR_reg <= 1'b0;
+                    PREADY_reg  <= 1'b1;
+                end
+                else if (PADDR == 4) begin
+                    PRDATA_reg <= status_1_reg;
+                    PSLVERR_reg <= 1'b0;
+                    PREADY_reg  <= 1'b1;
+                end
+                else if (PADDR == 8) begin
+                    PRDATA_reg <= status_2_reg;
+                    PSLVERR_reg <= 1'b0;
+                    PREADY_reg  <= 1'b1;
+                end
+                else if (PADDR == 12) begin
+                    PRDATA_reg <= status_3_reg;
+                    PSLVERR_reg <= 1'b0;
+                    PREADY_reg  <= 1'b1;
+                end
+                else begin
+                    PSLVERR_reg <= 1'b1;
+                    PREADY_reg  <= 1'b1;
+                end
+            end
+        end
+        else begin
+          PSLVERR_reg <= 1'b0;
+          PREADY_reg  <= 1'b0;
+        end
+    end
+
+  assign PSLVERR = PSLVERR_reg;
+  assign PRDATA  = PRDATA_reg;
+  assign PREADY  = PREADY_reg;
+
+endmodule
--- src/rtl/pmod_mux.sv
+++ src/rtl/pmod_mux.sv
@@ -1,25 +1,20 @@
 //-----------------------------------------------------------------------------
 // File          : pmod_mux.v
-// Creation date : 25.06.2024
-// Creation time : 14:29:15
-// Description   :
-// Created by    :
-// Tool : Kactus2 3.13.2 64-bit
-// Plugin : Verilog generator 2.4
 // This file was generated based on IP-XACT component tuni.fi:ip:pmod_mux:1.0
-// whose XML file is C:/Users/kayra/Documents/repos/Didactic-SoC/ipxact/tuni.fi/ip/pmod_mux/1.0/pmod_mux.1.0.xml
+// whose XML file is <repository>/ipxact/tuni.fi/ip/pmod_mux/1.0/pmod_mux.1.0.xml
 //-----------------------------------------------------------------------------
 /*
   Contributors:
     * Matti Käyrä (matti.kayra@tuni.fi)
   Description:
-    *
+    * mux/demux GPIO signals to various locations on chip
 */
 
 module pmod_mux #(
-    parameter IOCELL_CFG_W     = 5,    // control bus width for each individual IO cell
-    parameter IOCELL_COUNT     = 26,    // update this value manually to match cell numbers
-    parameter NUM_GPIO         = 8
+    parameter IOCELL_CFG_W = 5,  // control bus width for each individual IO cell
+    parameter IOCELL_COUNT = 26, // number of controllable cells
+    parameter NUM_GPIO     = 8,  // number of dedicated gpio cells
+    parameter NUM_SS       = 4   // number of subsystems on top level. TODO: combine ss vectors and make this even more generic
     )(
     // Interface: cell_cfg_from_core
     input  logic [IOCELL_COUNT*IOCELL_CFG_W-1:0] cell_cfg_from_core,
@@ -39,44 +34,25 @@ module pmod_mux #(
     input  logic [7:0]          pmod_sel,
 
     // Interface: ss_0_pmod_0
-    input  logic [3:0]          ss_0_pmod_0_gpio_oe,
-    input  logic [3:0]          ss_0_pmod_0_gpo,
-    output logic [3:0]          ss_0_pmod_0_gpi,
-
-    // Interface: ss_0_pmod_1
-    input  logic [3:0]          ss_0_pmod_1_gpio_oe,
-    input  logic [3:0]          ss_0_pmod_1_gpo,
-    output logic [3:0]          ss_0_pmod_1_gpi,
+    input  logic [NUM_GPIO-1:0]          ss_0_pmod_gpio_oe,
+    input  logic [NUM_GPIO-1:0]          ss_0_pmod_gpo,
+    output logic [NUM_GPIO-1:0]          ss_0_pmod_gpi,
 
     // Interface: ss_1_pmod_0
-    input  logic [3:0]          ss_1_pmod_0_gpio_oe,
-    input  logic [3:0]          ss_1_pmod_0_gpo,
-    output logic [3:0]          ss_1_pmod_0_gpi,
-
-    // Interface: ss_1_pmod_1
-    input  logic [3:0]          ss_1_pmod_1_gpio_oe,
-    input  logic [3:0]          ss_1_pmod_1_gpo,
-    output logic [3:0]          ss_1_pmod_1_gpi,
+    input  logic [NUM_GPIO-1:0]          ss_1_pmod_gpio_oe,
+    input  logic [NUM_GPIO-1:0]          ss_1_pmod_gpo,
+    output logic [NUM_GPIO-1:0]          ss_1_pmod_gpi,
 
     // Interface: ss_2_pmod_0
-    input  logic [3:0]          ss_2_pmod_0_gpio_oe,
-    input  logic [3:0]          ss_2_pmod_0_gpo,
-    output logic [3:0]          ss_2_pmod_0_gpi,
-
-    // Interface: ss_2_pmod_1
-    input  logic [3:0]          ss_2_pmod_1_gpio_oe,
-    input  logic [3:0]          ss_2_pmod_1_gpo,
-    output logic [3:0]          ss_2_pmod_1_gpi,
+    input  logic [NUM_GPIO-1:0]          ss_2_pmod_gpio_oe,
+    input  logic [NUM_GPIO-1:0]          ss_2_pmod_gpo,
+    output logic [NUM_GPIO-1:0]          ss_2_pmod_gpi,
 
     // Interface: ss_3_pmod_0
-    input  logic [3:0]          ss_3_pmod_0_gpio_oe,
-    input  logic [3:0]          ss_3_pmod_0_gpo,
-    output logic [3:0]          ss_3_pmod_0_gpi,
+    input  logic [NUM_GPIO-1:0]          ss_3_pmod_gpio_oe,
+    input  logic [NUM_GPIO-1:0]          ss_3_pmod_gpo,
+    output logic [NUM_GPIO-1:0]          ss_3_pmod_gpi
 
-    // Interface: ss_3_pmod_1
-    input  logic [3:0]          ss_3_pmod_1_gpio_oe,
-    input  logic [3:0]          ss_3_pmod_1_gpo,
-    output logic [3:0]          ss_3_pmod_1_gpi
 );
 
   // always direct gpi to controller core
@@ -89,90 +65,65 @@ module pmod_mux #(
     for(int i = 0; i < IOCELL_COUNT; i++) begin
       cell_cfg_to_io[(IOCELL_CFG_W*i+1)+:(IOCELL_CFG_W-1)] = cell_cfg_from_core[(IOCELL_CFG_W*i+1)+:(IOCELL_CFG_W-1)];
     end
-    for(int i = NUM_GPIO; i < IOCELL_COUNT; i++) begin
-      cell_cfg_to_io[IOCELL_CFG_W*i] = cell_cfg_from_core[IOCELL_CFG_W*i];
-    end
+//    for(int i = NUM_GPIO; i < IOCELL_COUNT; i++) begin
+//      cell_cfg_to_io[IOCELL_CFG_W*i] = cell_cfg_from_core[IOCELL_CFG_W*i];
+//    end
 
     unique case(pmod_sel)
 
       0: begin
-        gpio_to_io = {ss_0_pmod_1_gpo,ss_0_pmod_0_gpo};
-        for(int i = 0; i < 4; i++) begin
-          cell_cfg_to_io[i*IOCELL_CFG_W]                = ss_0_pmod_0_gpio_oe[i];
-          cell_cfg_to_io[4*IOCELL_CFG_W+i*IOCELL_CFG_W] = ss_0_pmod_1_gpio_oe[i];
+        gpio_to_io = ss_0_pmod_gpo;
+        for(int i = NUM_GPIO; i > 0; i--) begin
+          cell_cfg_to_io[9*IOCELL_CFG_W+i*IOCELL_CFG_W] = ss_0_pmod_gpio_oe[i];
         end
-        ss_0_pmod_0_gpi = gpio_from_io[3:0];
-        ss_0_pmod_1_gpi = gpio_from_io[7:0];
-        ss_1_pmod_0_gpi = 'h0;
-        ss_1_pmod_1_gpi = 'h0;
-        ss_2_pmod_0_gpi = 'h0;
-        ss_2_pmod_1_gpi = 'h0;
-        ss_3_pmod_0_gpi = 'h0;
-        ss_3_pmod_1_gpi = 'h0;
+        ss_0_pmod_gpi = gpio_from_io;
+        ss_1_pmod_gpi = 'h0;
+        ss_2_pmod_gpi = 'h0;
+        ss_3_pmod_gpi = 'h0;
       end
 
       1: begin
-        gpio_to_io = {ss_1_pmod_1_gpo,ss_1_pmod_0_gpo};
-        for(int i = 0; i < 4; i++) begin
-          cell_cfg_to_io[i*IOCELL_CFG_W]                = ss_1_pmod_0_gpio_oe[i];
-          cell_cfg_to_io[4*IOCELL_CFG_W+i*IOCELL_CFG_W] = ss_1_pmod_1_gpio_oe[i];
+        gpio_to_io = ss_1_pmod_gpo;
+        for(int i = NUM_GPIO; i > 0; i--) begin
+          cell_cfg_to_io[9*IOCELL_CFG_W+i*IOCELL_CFG_W] = ss_1_pmod_gpio_oe[i];
         end
-        ss_0_pmod_0_gpi = 'h0;
-        ss_0_pmod_1_gpi = 'h0;
-        ss_1_pmod_0_gpi = gpio_from_io[3:0];
-        ss_1_pmod_1_gpi = gpio_from_io[7:0];
-        ss_2_pmod_0_gpi = 'h0;
-        ss_2_pmod_1_gpi = 'h0;
-        ss_3_pmod_0_gpi = 'h0;
-        ss_3_pmod_1_gpi = 'h0;
+        ss_0_pmod_gpi = 'h0;
+        ss_1_pmod_gpi = gpio_from_io;
+        ss_2_pmod_gpi = 'h0;
+        ss_3_pmod_gpi = 'h0;
       end
 
       2: begin
-        gpio_to_io = {ss_2_pmod_1_gpo,ss_2_pmod_0_gpo};
-        for(int i = 0; i < 4; i++) begin
-          cell_cfg_to_io[i*IOCELL_CFG_W]                = ss_2_pmod_0_gpio_oe[i];
-          cell_cfg_to_io[4*IOCELL_CFG_W+i*IOCELL_CFG_W] = ss_2_pmod_1_gpio_oe[i];
+        gpio_to_io = ss_2_pmod_gpo;
+        for(int i = NUM_GPIO; i > 0; i--) begin
+          cell_cfg_to_io[9*IOCELL_CFG_W+i*IOCELL_CFG_W] = ss_2_pmod_0_gpio_oe[i];
         end
-        ss_0_pmod_0_gpi = 'h0;
-        ss_0_pmod_1_gpi = 'h0;
-        ss_1_pmod_0_gpi = 'h0;
-        ss_1_pmod_1_gpi = 'h0;
-        ss_2_pmod_0_gpi = gpio_from_io[3:0];
-        ss_2_pmod_1_gpi = gpio_from_io[7:0];
-        ss_3_pmod_0_gpi = 'h0;
-        ss_3_pmod_1_gpi = 'h0;
+        ss_0_pmod_gpi = 'h0;
+        ss_1_pmod_gpi = 'h0;
+        ss_2_pmod_gpi = gpio_from_io;
+        ss_3_pmod_gpi = 'h0;
       end
 
       3: begin
-        gpio_to_io = {ss_3_pmod_1_gpo,ss_3_pmod_0_gpo};
-        for(int i = 0; i < 4; i++) begin
-          cell_cfg_to_io[i*IOCELL_CFG_W]                = ss_3_pmod_0_gpio_oe[i];
-          cell_cfg_to_io[4*IOCELL_CFG_W+i*IOCELL_CFG_W] = ss_3_pmod_1_gpio_oe[i];
+        gpio_to_io = ss_3_pmod_gpo;
+        for(int i = NUM_GPIO; i > 0; i--) begin
+          cell_cfg_to_io[9*IOCELL_CFG_W+i*IOCELL_CFG_W] = ss_3_pmod_0_gpio_oe[i];
         end
-        ss_0_pmod_0_gpi = 'h0;
-        ss_0_pmod_1_gpi = 'h0;
-        ss_1_pmod_0_gpi = 'h0;
-        ss_1_pmod_1_gpi = 'h0;
-        ss_2_pmod_0_gpi = 'h0;
-        ss_2_pmod_1_gpi = 'h0;
-        ss_3_pmod_0_gpi = gpio_from_io[3:0];
-        ss_3_pmod_1_gpi = gpio_from_io[7:0];
+        ss_0_pmod_gpi = 'h0;
+        ss_1_pmod_gpi = 'h0;
+        ss_2_pmod_gpi = 'h0;
+        ss_3_pmod_gpi = gpio_from_io;
       end
 
       default: begin
         gpio_to_io = gpio_from_core;
-        for(int i = 0; i < 4; i++) begin
-          cell_cfg_to_io[i*IOCELL_CFG_W]                = cell_cfg_from_core[i*IOCELL_CFG_W];
-          cell_cfg_to_io[4*IOCELL_CFG_W+i*IOCELL_CFG_W] = cell_cfg_from_core[4*IOCELL_CFG_W+i*IOCELL_CFG_W];
+        for(int i = NUM_GPIO; i > 0; i--) begin
+          cell_cfg_to_io[9*IOCELL_CFG_W+i*IOCELL_CFG_W] = cell_cfg_from_core[9*IOCELL_CFG_W+i*IOCELL_CFG_W];
         end
-        ss_0_pmod_0_gpi = 'h0;
-        ss_0_pmod_1_gpi = 'h0;
-        ss_1_pmod_0_gpi = 'h0;
-        ss_1_pmod_1_gpi = 'h0;
-        ss_2_pmod_0_gpi = 'h0;
-        ss_2_pmod_1_gpi = 'h0;
-        ss_3_pmod_0_gpi = 'h0;
-        ss_3_pmod_1_gpi = 'h0;
+        ss_0_pmod_gpi = 'h0;
+        ss_1_pmod_gpi = 'h0;
+        ss_2_pmod_gpi = 'h0;
+        ss_3_pmod_gpi = 'h0;
       end
 
     endcase
