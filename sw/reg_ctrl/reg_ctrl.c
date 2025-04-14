d00496defca0474343e4d69c6816e9c7ea0d0f01
Matti Käyrä <matti.kayra@tuni.fi>
2025-04-14 15:40:08 +0300

    [sw] update reg ctrl test case to have analog ss registers accessible

--- sw/reg_ctrl/reg_ctrl.c
+++ sw/reg_ctrl/reg_ctrl.c
@@ -19,18 +19,19 @@ int main(){
   ss_init(1);
   ss_init(2);
   ss_init(3);
+  ss_init(4);
   
-  ss_reset(4);//unused index = reset all
+  ss_reset(5);//unused index = reset all
 
  // uart_print("initial reset round done");
 
   for(uint32_t i=0; i<5; i++){
-    pmod_target(i);
+    pmod_target(i);// only 4 pmod targets exist currently
   }
   
 //  uart_print("pmod loop");
 
-  for (uint32_t i=0; i<5; i++){
+  for (uint32_t i=0; i<6; i++){
     ss_init(i);
     ss_reset(i);
     ss_init_high_speed(i);
@@ -43,7 +44,7 @@ int main(){
   volatile uint32_t read_val=0;
   volatile uint32_t ones= 0xFFFFFFFF;
 
-  for(uint32_t i=0; i < 27; i++ ){
+  for(uint32_t i=0; i < 35; i++ ){
     *(volatile uint32_t*)(CTRL_BASE+i*4)=ones;
     read_val=*(volatile uint32_t*)(CTRL_BASE+i*4);
     if (read_val!=ones){
