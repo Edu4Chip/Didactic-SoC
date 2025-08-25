706bb91352b7a4364e183a9ec46da3a9e52aecc5
Antti Nurmi <antti.nurmi@tuni.fi>
2025-08-25 15:31:05 +0300

    Add memtest.c

---
+++ sw/memtest/memtest.c
@@ -0,0 +1,13 @@
+/*
+ * Name: memtest.c
+ * Contributor(s):
+ *    - Antti Nurmi (antti.nurmi@tuni.fi) 
+ * Description:
+ *    - Dummy program for JTAG-based memory test
+ */
+
+int main() {
+  while(1)
+    ;
+  return 0;
+}
